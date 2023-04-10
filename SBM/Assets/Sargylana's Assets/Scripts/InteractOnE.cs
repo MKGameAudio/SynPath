a619a73de218c925d8f38370f1022cf15c5c5904
multicolorcolor <112100252+multicolorcolor@users.noreply.github.com>
2023-04-10 00:11:14 -0500

    Squashed commit of the following:
    commit 2cb3aa35a785abbf13534a220f2fefe5df72793d
    Author: multicolorcolor <112100252+multicolorcolor@users.noreply.github.com>
    Date:   Sun Apr 9 23:56:34 2023 -0500
    
        Updated textures, animation, geometry, new objects, new interactions

---
+++ SBM/Assets/Sargylana's Assets/Scripts/InteractOnE.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractOnE : MonoBehaviour
+{
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+    public UnityEvent interactAction01;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(interactKey))
+        {
+            interactAction.Invoke();
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            isInRange = true;
+
+
+
+        }
+
+    }
+    private void OnTriggerExit(Collider collision)
+
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+            interactAction01.Invoke();
+        }
+
+    }
+
+}
+
